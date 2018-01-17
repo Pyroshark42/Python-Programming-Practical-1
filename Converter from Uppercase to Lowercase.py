133d60c0acddd3f3d27054fae14a00aff038493d
Pyroshark42 <35279485+Pyroshark42@users.noreply.github.com>
2018-01-17 23:06:45 +0800

    Add files via upload

---
+++ Converter from Uppercase to Lowercase.py
@@ -0,0 +1,8 @@
+# Converter from Uppercase to Lowercase
+
+# Input
+name = input("Enter name: ")
+
+
+# changing Uppercase to Lowercase
+print(name.lower())
---
+++ Selection of ASCII character from integer.py
@@ -0,0 +1,8 @@
+# Selection of ASCII code factor in set
+
+# Input of number
+number = int(input("Please enter a number to get your ASCII code: "))
+
+# Retrieving the code corresponding to number
+c = chr(number)
+print(c)
